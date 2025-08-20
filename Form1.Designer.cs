cd6a95789bd778a88d6ab551c581e1eb9c0e9bff
shyam4545 <154552755+shyam4545@users.noreply.github.com>
2025-08-20 19:28:19 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace NetworkTrafficAnalyzer_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.Packets = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(12, 12);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(238, 24);
+            this.cmbDevices.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnStart.Location = new System.Drawing.Point(272, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(163, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Capture";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(441, 13);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(145, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop Capture";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // Packets
+            // 
+            this.Packets.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.Packets.FullRowSelect = true;
+            this.Packets.HideSelection = false;
+            this.Packets.Location = new System.Drawing.Point(12, 63);
+            this.Packets.Name = "Packets";
+            this.Packets.Size = new System.Drawing.Size(513, 298);
+            this.Packets.TabIndex = 3;
+            this.Packets.UseCompatibleStateImageBehavior = false;
+            this.Packets.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "No.";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Time";
+            this.columnHeader2.Width = 69;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Source";
+            this.columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Destination";
+            this.columnHeader4.Width = 102;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Protocol";
+            this.columnHeader5.Width = 79;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Length";
+            this.columnHeader6.Width = 93;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 438);
+            this.Controls.Add(this.Packets);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cmbDevices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListView Packets;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using PacketDotNet;
+using PacketDotNet.Ieee80211;
+using SharpPcap;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace NetworkTrafficAnalyzer_Project
+{
+    public partial class Form1 : Form
+    {
+        // A class-level variable to hold the list of network devices.
+        private CaptureDeviceList devices;
+        // The currently selected device for capturing.
+        private ICaptureDevice selectedDevice;
+        // A counter for the packet number.
+        private int packetCounter = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDeviceList();
+        }
+
+        /// <summary>
+        /// Populates the ComboBox with the list of available network devices.
+        /// </summary>
+        private void InitializeDeviceList()
+        {
+            // Retrieve the device list
+            devices = CaptureDeviceList.Instance;
+
+            // Ensure that devices were found
+            if (devices.Count < 1)
+            {
+                MessageBox.Show("No network devices found. Make sure Npcap is installed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+
+            // Add each device to the combo box
+            foreach (var dev in devices)
+            {
+                // The description is usually more human-readable than the name.
+                cmbDevices.Items.Add(dev.Description);
+            }
+
+            // Select the first device by default.
+            if (cmbDevices.Items.Count > 0)
+            {
+                cmbDevices.SelectedIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event for the "Start Capture" button.
+        /// </summary>
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the selected device from the ComboBox.
+                selectedDevice = devices[cmbDevices.SelectedIndex];
+
+                // Register our event handler for packet arrivals.
+                selectedDevice.OnPacketArrival += OnPacketArrival;
+
+                // Open the device for capturing.
+                // promiscuous mode  : true means the device captures all packets on the network, not just those addressed to it.
+                // read timeout (ms) : 1000 ms = 1 second.
+                int readTimeoutMilliseconds = 1000;
+                selectedDevice.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+
+                // Start the capture process.
+                selectedDevice.StartCapture();
+
+                // Update UI state.
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                cmbDevices.Enabled = false;
+                Packets.Items.Clear(); // Clear previous captures
+                packetCounter = 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting capture: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event for the "Stop Capture" button.
+        /// </summary>
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedDevice != null && selectedDevice.Started)
+                {
+                    // Stop the capture process.
+                    selectedDevice.StopCapture();
+                    // Close the device.
+                    selectedDevice.Close();
+                    // Unregister the event handler.
+                    selectedDevice.OnPacketArrival -= OnPacketArrival;
+                }
+            }
+            catch (Exception ex)
+            {
+                // It's good practice to handle potential exceptions, though less common here.
+                MessageBox.Show($"Error stopping capture: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Update UI state.
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                cmbDevices.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// This is the core method that gets called for each captured packet.
+        /// </summary>
+        private void OnPacketArrival(object sender, PacketCapture e)
+        {
+            // ** FIX **
+            // We must copy the data from the packet capture 'e' into local variables
+            // before we enter the Invoke delegate. This is because 'e' is a ref-like
+            // type and cannot be used in a lambda expression.
+            var rawPacket = e.GetPacket();
+            var timestamp = e.Header.Timeval.Date;
+
+            // This method runs on a background thread, so we need to use Invoke
+            // to update the UI elements, which are on the main thread.
+            this.Invoke((MethodInvoker)delegate
+            {
+                try
+                {
+                    // Use the local variables (rawPacket, timestamp) inside the Invoke block.
+                    var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+
+                    // Default values
+                    string sourceIp = "N/A";
+                    string destIp = "N/A";
+                    string protocol = "N/A";
+
+                    // Extract IP packet information
+                    // ** FIX: Corrected IPacket to IPPacket **
+                    var ipPacket = packet.Extract<IPPacket>();
+                    if (ipPacket != null)
+                    {
+                        sourceIp = ipPacket.SourceAddress.ToString();
+                        destIp = ipPacket.DestinationAddress.ToString();
+                        protocol = ipPacket.Protocol.ToString();
+                    }
+
+                    // Create a new ListView item
+                    var item = new ListViewItem(packetCounter.ToString());
+                    item.SubItems.Add(timestamp.ToLongTimeString()); // Use the copied timestamp
+                    item.SubItems.Add(sourceIp);
+                    item.SubItems.Add(destIp);
+                    item.SubItems.Add(protocol);
+                    item.SubItems.Add(rawPacket.Data.Length.ToString());
+
+                    // Add the item to the ListView
+                    Packets.Items.Add(item);
+
+                    // Increment the packet counter
+                    packetCounter++;
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions during packet processing.
+                    // This can prevent the application from crashing on malformed packets.
+                    Console.WriteLine($"Packet processing error: {ex.Message}");
+                }
+            });
+        }
+
+        /// <summary>
+        /// Ensures the capture is stopped when the form is closed.
+        /// </summary>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (selectedDevice != null && selectedDevice.Started)
+            {
+                BtnStop_Click(null, null); // Gracefully stop the capture.
+            }
+        }
+    }
+}
